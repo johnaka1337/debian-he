14a92b6363903b73fde32d66e3b042e468d78f13
John Crew <johnaka1337@gmail.com>
2026-02-28 17:11:33 +0500

    Update sign_krenel.sh

--- var/lib/dkms/sign_krenel.sh
+++ var/lib/dkms/sign_krenel.sh
@@ -1,5 +1,5 @@
 #ver=$(uname -r)
-ver=6.19.5-1000hz
+ver=6.19.5-lazy-1000hz
 echo "Kernel version: $ver"
 sbsign --key mok.key --cert mok.pem /boot/vmlinuz-$ver --output /tmp/vmlinuz
 mv /tmp/vmlinuz /boot/vmlinuz-$ver
