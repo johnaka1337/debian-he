3ec9908c266eb7a50f70da3d216660d006b7e461
John Crew <johnaka1337@gmail.com>
2026-04-24 23:10:32 +0500

    feat(config): update config v7.0.1 fix(intall): fixed step of fetching vanilla sources

--- 03-fetch_source.sh
+++ 03-fetch_source.sh
@@ -10,9 +10,9 @@ if [ "$KERNEL_TYPE" = "vanilla" ]; then
 
     JSON_DATA=$(wget -qO- https://www.kernel.org/releases.json)
 
-    KERNEL_VERSION=$(echo "$JSON_DATA" | jq -r '.releases[] | select(.moniker=="stable") | .version')
-    DOWNLOAD_URL=$(echo "$JSON_DATA" | jq -r '.releases[] | select(.moniker=="stable") | .source')
-    PGP_URL=$(echo "$JSON_DATA" | jq -r '.releases[] | select(.moniker=="stable") | .pgp')
+    KERNEL_VERSION=$(echo "$JSON_DATA" | jq -r '[.releases[] | select(.moniker=="stable")] | first | .version')
+    DOWNLOAD_URL=$(echo "$JSON_DATA" | jq -r '[.releases[] | select(.moniker=="stable")] | first | .source')
+    PGP_URL=$(echo "$JSON_DATA" | jq -r '[.releases[] | select(.moniker=="stable")] | first | .pgp')
 
     if [ -z "$KERNEL_VERSION" ] || [ "$KERNEL_VERSION" == "null" ]; then
         echo "Error: Could not parse stable kernel version from JSON."
@@ -20,12 +20,27 @@ if [ "$KERNEL_TYPE" = "vanilla" ]; then
     fi
 
     echo "==> Latest stable is ${KERNEL_VERSION}"
-    echo "==> Downloading tarball..."
+    echo "==> Downloading tarball and signature..."
     wget -c "$DOWNLOAD_URL"
+    wget -nc "$PGP_URL"
 
-    #TODO check signature
-    #echo "==> Downloading PGP signature..."
-    #wget -q "$PGP_URL"
+    SIG_FILE=$(basename "$PGP_URL")
+    TAR_FILE=$(basename "$DOWNLOAD_URL")
+
+    #echo "==> Verifying PGP signature..."
+    # import public keys
+    #gpg --locate-keys torvalds@kernel.org gregkh@kernel.org >/dev/null 2>&1
+
+    # Checking. 0 == errors
+    #if gpg --verify "$SIG_FILE" "$TAR_FILE" >/dev/null 2>&1; then
+    #    echo "==> Signature check: PASSED"
+    #else
+    #    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    #    echo "CRITICAL ERROR: PGP Signature verification failed!"
+    #    echo "The source code might be corrupted or tampered with."
+    #    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    #    exit 1
+    #fi
 
     echo "==> Unpacking vanilla archive..."
     tar -xf "linux-${KERNEL_VERSION}.tar.xz"
